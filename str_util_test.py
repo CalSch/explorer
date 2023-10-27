077482e7fc16c853c4c30a2646ba843164ea8ccf
CalSch <cal.schwick@gmail.com>
2023-10-27 17:30:38 -0500

    new demo thing

--- str_util_test.py
+++ str_util_test.py
@@ -9,8 +9,25 @@ but with more lines
 what will happen?
 nobody knows!"""
 
-print(su.justify("left",20,su.TextJustify.Left))
-print(su.justify("center",20,su.TextJustify.Center))
-print(su.justify("right",20,su.TextJustify.Right))
+print("\n=== Justifying ===")
+print("+"+"-"*20+"+")
+print("|"+su.justify("left",20,su.TextJustify.Left)+"|")
+print("|"+su.justify("center",20,su.TextJustify.Center)+"|")
+print("|"+su.justify("right",20,su.TextJustify.Right)+"|")
+print("+"+"-"*20+"+")
 
-print(su.join_horizontal(str1,str2))
+print("\n\n=== Join Horizontal ===")
+print(f"First string:\n{str1}\n")
+print(f"Second string:\n{str2}\n")
+print("Combined (with padding=5):")
+print(su.join_horizontal(str1,str2,padding=5))
+
+print("\n\n")
+print(
+    su.join_horizontal(
+        su.join_horizontal(
+            str1," | \n"*5
+        ),
+        str2
+    )
+)
