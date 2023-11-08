15ed4a3f81bb3294031e46ad52b44de5b6bd062d
CalSch <cal.schwick@gmail.com>
2023-11-08 08:56:34 -0600

    added TextInput component

---
+++ text_input.py
@@ -0,0 +1,82 @@
+import component
+import string_util as su
+import colors
+import keys
+from typing import Callable
+
+class TextInput(component.Component):
+    def __init__(self,
+                 width: int,
+                 height: int,
+                 prompt: str="",
+                 placeholder: str="",
+                 name: str="TextInput",
+                 use_border: bool=False
+                ):
+        super().__init__(width, height, name)
+        self.use_border=use_border
+        self.prompt=prompt
+        self.placeholder=placeholder
+        self.text=""
+        self.cursor=0
+    
+    def onsubmit(self):
+        pass
+
+    def setonsubmit(self,func:Callable):
+        self.onsubmit=func
+
+    def updateCursor(self):
+        self.cursor = max(0,min(len(self.text),self.cursor))
+
+    def input(self, text: str):
+        if text == keys.left:
+            self.cursor -= 1
+        elif text == keys.right:
+            self.cursor += 1
+        elif text == keys.backspace:
+            if self.cursor!=0:
+                self.text=self.text[:self.cursor-1] + self.text[self.cursor:] # remove the character at `self.cursor-1`
+                self.cursor -= 1
+        elif text == keys.delete:
+            if self.cursor!=len(self.text):
+                self.text=self.text[:self.cursor] + self.text[self.cursor+1:] # remove the character at `self.cursor-1`
+        elif text == keys.enter or text == keys.cr:
+            self.onsubmit(self)
+        elif text in [keys.up,keys.down]:
+            pass
+        else:
+            self.text = self.text[:self.cursor] + text + self.text[self.cursor:] # insert the character into `self.cursor`
+            self.cursor += len(text)
+        self.updateCursor()
+    
+    def view(self) -> str:
+        s = ""
+        s += " "
+        s += self.prompt
+        s += " "
+        text = self.text+" " if self.text else self.placeholder
+
+        if not self.text:
+            s += colors.dim.on
+
+        s += text[:self.cursor]
+        if self.focused:
+            s += colors.invert.on
+        s += text[self.cursor] # highlight the cursor position
+        if self.focused:
+            s += colors.invert.off
+        s += text[self.cursor+1:]
+
+        if not self.text:
+            s += colors.dim.off
+        # else:
+        #     s += colors.dim.on + self.placeholder + colors.dim.off
+        s += " "
+        if self.text:
+            s += f"\n{repr(self.text)}"
+        
+        if self.use_border:
+            s = su.border(s,su.round_border)
+        
+        return s
---
+++ text_input_demo.py
@@ -0,0 +1,78 @@
+import text_input
+import sys,termios,tty,select
+import keys
+
+#region input stuff
+
+stdin_fd = sys.stdin.fileno()
+# print(f"stdin: {stdin_fd}")
+# quit(0)
+try:
+    old_term_settings = termios.tcgetattr(stdin_fd)
+except:
+    old_term_settings = None
+
+def reset_term():
+    if old_term_settings == None:
+        return
+    termios.tcsetattr(stdin_fd, termios.TCSADRAIN, old_term_settings)
+
+def getchar(timeout:float=0.01):
+    ch = ""
+    #Returns a single character from standard input
+    try:
+        tty.setraw(stdin_fd,when=termios.TCSANOW)
+        # ch = sys.stdin.read(1)
+        i, o, e = select.select( [sys.stdin], [], [], timeout)
+        # print(i)
+        if i:
+            ch = (sys.stdin.buffer.raw.read(1)).decode('utf-8')
+            # print(repr(ch))
+            # time.sleep(0.5)
+        else:
+            return None
+    finally:
+        reset_term()
+    return ch
+
+def getstr():
+    s = ""
+    ret = ""
+    timeout=1 # the first getchar has a big timeout
+    while ret != None:
+        s += ret
+        ret = getchar(timeout)
+        timeout=0.01 # subsequent getchars have a small timeout
+    return s
+
+#endregion
+
+ti = text_input.TextInput(
+    width=30,
+    height=1,
+    prompt="Type stuff:",
+    placeholder="cool placeholder",
+    use_border=False
+)
+
+ti.onfocus()
+
+@ti.setonsubmit
+def onsubmit(self:text_input.TextInput):
+    global running
+    running=False
+    print("Submitted!")
+    print(f"'{self.text}'")
+
+
+running=True
+while running:
+    print("\x1b[2J\x1b[H")
+    print("Press escape to exit")
+    print(ti.view())
+    print("\n\n")
+    text=getstr()
+    if text==keys.escape:
+        running=False
+    if text!="":
+        ti.input(text)
