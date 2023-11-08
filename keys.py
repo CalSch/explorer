44a51fabcaa6ce323d0f947a1950f8360eb2c794
CalSch <cal.schwick@gmail.com>
2023-11-08 08:54:42 -0600

    added new keys

--- keys.py
+++ keys.py
@@ -2,8 +2,19 @@ up   ="\x1b[A"
 down ="\x1b[B"
 left ="\x1b[D"
 right="\x1b[C"
-enter="\n"
+
+ctrl_up   ="\x1b[1;5A"
+ctrl_down ="\x1b[1;5B"
+ctrl_left ="\x1b[1;5D"
+ctrl_right="\x1b[1;5C"
+
+enter="\r"
+linefeed="\n"
+
 page_up="\x1b[5~"
 page_down="\x1b[6~"
+
 backspace="\x7f"
+delete="\x1b[3~"
+
 escape="\x1b"
